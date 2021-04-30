8e984edec615654be9cd83d4b7eb285597caf64a
Codeco <adriel.codeco@raizen.com>
2021-04-30 11:44:22 -0300

    fix: logs

--- packages/framework/src/infrastructure/logger/consoleLogger.ts
+++ packages/framework/src/infrastructure/logger/consoleLogger.ts
@@ -1,6 +1,9 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import { injectable } from 'inversify'
 import { transports } from 'winston'
 import { LoggerClass } from './logger'
+
+const { LEVEL, MESSAGE } = require('triple-beam')
 
 @injectable()
 export class ConsoleLogger extends LoggerClass {
@@ -8,6 +11,26 @@ export class ConsoleLogger extends LoggerClass {
     super([
       new transports.Console({
         handleExceptions: true,
+        log (info, callback) {
+          // eslint-disable-next-line @typescript-eslint/no-this-alias
+          const self: any = this
+          setImmediate(() => self.emit('logged', info))
+
+          if (self.stderrLevels[info[LEVEL]]) {
+            console.error(info[MESSAGE])
+
+            if (callback) {
+              callback()
+            }
+            return
+          }
+
+          console.log(info[MESSAGE])
+
+          if (callback) {
+            callback()
+          }
+        },
       }),
     ])
   }
